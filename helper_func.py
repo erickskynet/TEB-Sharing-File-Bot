8773d025d8bba82ca7790f87a60bfbb769393e09
SKY <erickskynet@users.noreply.github.com>
2021-10-07 02:55:10 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -4,7 +4,7 @@ import base64
 import re
 import asyncio
 from pyrogram import filters
-from config import FORCE_SUB_CHANNEL, ADMINS
+from config import FORCE_SUB_CHANNEL, FORCE_SUB_GROUP, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
@@ -23,7 +23,22 @@ async def is_subscribed(filter, client, message):
         return False
     else:
         return True
+    
+    if not FORCE_SUB_GROUP:
+        return True
+    user_id = message.from_user.id
+    if user_id in ADMINS:
+        return True
+    try:
+        member = await client.get_chat_member(chat_id = FORCE_SUB_GROUP, user_id = user_id)
+    except UserNotParticipant:
+        return False
 
+    if not member.status in ["creator", "administrator", "member"]:
+        return False
+    else:
+        return True
+    
 async def encode(string):
     string_bytes = string.encode("ascii")
     base64_bytes = base64.b64encode(string_bytes)
